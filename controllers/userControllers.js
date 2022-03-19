0d123946c6592855677bf8dd1ada6dca1dc7851a
AlphaAnimal <h.h.redondo@gmail.com>
2022-03-19 15:01:06 +0100

    Controller, Routes, Errors set up

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,40 @@
-const getUsers = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+//@desc Get users
+//@route GET /api/users
+//@access Private
+const getUsers = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get users'})
-}
+})
+
+//@desc Set users
+//@route SET /api/users
+//@access Private
+const setUser = asyncHandler(async (req, res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field to the body')
+    }
+    res.status(200).json({message: 'Set users'})
+})
+
+//@desc Update users
+//@route PUT /api/users/id
+//@access Private
+const updateUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message: `Update user ${req.params.id}`})
+})
+
+//@desc Delete users
+//@route DELETE /api/users:id
+//@access Private 
+const deleteUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message: `Delete user ${req.params.id}`})
+})
 
 module.exports = {
-    getUsers
+    getUsers,
+    setUser,
+    updateUser,
+    deleteUser,
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,20 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
-const {getUsers} = require('../controllers/userControllers')
+const { getUsers, setUser, updateUser, deleteUser  } = require('../controllers/userControllers')
 
-router.get('/', getUsers)
+router.route('/').get(getUsers).post(setUser)
+router.route('/:id').put(updateUser).delete(deleteUser)
 
-router.post('/', (req, res) => {
-    res.status(200).json({message: 'Post users'})
-})
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({message: `Update user ${req.params.id}`})
-})
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({message: `Delete user ${req.params.id}`})
-})
 
 module.exports = router
