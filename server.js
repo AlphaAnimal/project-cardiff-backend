5819f54bb5101425fba978ce3a3b7fa3f28aaa87
AlphaAnimal <h.h.redondo@gmail.com>
2022-04-26 20:15:20 +0200

    register working lololol

--- server.js
+++ server.js
@@ -8,12 +8,22 @@ connectDB()
 
 const app = express()
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/words', require('./routes/wordRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/scores', require('./routes/scoreRoutes'))
+
 
 
 
